3b81f0b80d99b2068cc16f23db69b2380dbb29da
MarcoWishraiser <marco.lisotta@wishraiser.com>
2023-01-09 16:03:04 +0100

    Added table giornata

--- JavaScript/myScripts.js
+++ JavaScript/myScripts.js
@@ -1,6 +1,6 @@
-let url = "https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=Italian%20Lega%20Basket";
+let url_teams = "https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=Italian%20Lega%20Basket";
 
-fetch(url)
+fetch(url_teams)
     .then(response => response.json())
     .then(data => teamsNames(data["teams"]));
 
@@ -24,4 +24,35 @@ function teamsNames(teams) {
         list.appendChild(li);
     })
 
+}
+
+let url = "https://www.thesportsdb.com/api/v1/json/2/eventsround.php?id=4433&r=1&s=2022-2023";
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => ranks(data["events"]));
+
+
+function ranks(events) {
+    console.log(events);
+
+    let giornata = document.getElementById("giornata");
+
+    events.map(function(event) {
+        let tr = document.createElement("tr");
+
+        let home = document.createElement("td");
+        home.innerText = event.strHomeTeam;
+        tr.appendChild(home);
+
+        let result = document.createElement("td");
+        result.innerText = event.intHomeScore + " - " + event.intAwayScore;
+        tr.appendChild(result);
+
+        let guest = document.createElement("td");
+        guest.innerText = event.strAwayTeam;
+        tr.appendChild(guest);
+
+        giornata.appendChild(tr);
+    })
 }
