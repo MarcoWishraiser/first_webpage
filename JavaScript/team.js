36b2199b7f680c9dfd7df5acc72370aa375238b5
MarcoWishraiser <marco.lisotta@wishraiser.com>
2023-01-12 16:44:15 +0100

    implementation team page

--- JavaScript/team.js
+++ JavaScript/team.js
@@ -16,13 +16,60 @@ function printTeamData(teams) {
     const team = teams.find(team => team.idTeam === intId);
     console.log(team);
 
-    let img_logo = document.getElementById("team-logo");
-    img_logo.setAttribute("src", team.strTeamBadge);
+    let imgLogo = document.getElementById("team-logo");
+    imgLogo.setAttribute("src", team.strTeamBadge);
 
     let h2 = document.getElementById("team-name");
     h2.innerText = team.strTeam;
 
-    let descrizione = document.getElementById("descrizione");
-    descrizione.innerText = team.strDescriptionIT;
+    let description = document.getElementById("description");
+    description.innerText = team.strDescriptionIT;
 
+    /*let imgStadium = document.getElementById("stadium-image");
+    imgStadium.setAttribute("src", team.strStadiumThumb);
+
+    let stadiumDescription = document.getElementById("stadium-description");
+    stadiumDescription.innerText = team.strStadiumDescription;*/
+
+}
+
+
+let url_events = `https://www.thesportsdb.com/api/v1/json/2/eventslast.php?id=${intId}`;
+
+fetch(url_events)
+    .then(response => response.json())
+    .then(data => latestResults(data["results"]));
+
+
+function latestResults(results) {
+    console.log(results);
+
+    let table = document.getElementById("latest-results");
+
+    results.map(function(result) {
+        let tr = document.createElement("tr");
+
+
+        let date = document.createElement("td");
+        date.innerText = result.dateEvent;
+        tr.appendChild(date);
+
+        let home = document.createElement("td");
+        home.innerText = result.strHomeTeam;
+        tr.appendChild(home);
+
+        let score = document.createElement("td");
+        if (result.intHomeScore === null) {
+            score.innerText = `0 - 0`
+        } else {
+            score.innerText = result.intHomeScore + " - " + result.intAwayScore;
+        }
+        tr.appendChild(score);
+
+        let away = document.createElement("td");
+        away.innerText = result.strAwayTeam;
+        tr.appendChild(away);
+
+        table.appendChild(tr);
+    });
 }
